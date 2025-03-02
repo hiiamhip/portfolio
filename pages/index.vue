5c5cc1269e902f43f1c8f805bbf4933f9f16543b
hiiamhip <trantuanhiep420@gmail.com>
2025-03-02 08:43:02 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<section>
+    <h1 class="text-5xl font-bold mt-20">👋🏻 Hello, I'm Dan Vega!</h1>
+    <p class="text-base text-gray-900 p-2 italic">Spring Developer Advocate at VMware</p>
+  </section>
+	<section class="flex flex-col md:flex-row mt-8">
+    <div class="md:w-3/4">
+      <h2 class="text-3xl font-bold">My Story</h2>
+      <p class="text-lg py-2">
+        I am a Software Engineer living just outside of one of my favorite places in the world, Cleveland, Ohio. I am a
+        self-taught programmer who firmly believes that you can accomplish anything in this life if you put your mind to
+        it,
+        roll up your sleeves and are willing to put in the work.
+      </p>
+      <p class="text-lg py-2">
+        I have been writing software for over 20+ years now and I use my knowledge, passion, and influence to help other
+        developers achieve their goals. I teach online and currently, have over 160,000 students.
+      </p>
+      <p class="text-lg py-2">
+        In my personal life I am a husband to my best friend in the world and a father of two little girls. I enjoy
+        reading,
+        running and lifting weights to get me through the week.
+      </p>
+    </div>
+		<div class="md:w-1/4">
+			<img src="../assets/images/hip-smile.jpg" alt="" class="w-full rounded-full object-cover aspect-square">
+		</div>
+  </section>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
